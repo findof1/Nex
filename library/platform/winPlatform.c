d155b04daec8c5a34b001c5a3b0ba44e884d7ba4
Findof <joeargon21@gmail.com>
2025-04-11 09:34:21 -0400

    Finished platform.h for now

--- library/platform/winPlatform.c
+++ library/platform/winPlatform.c
@@ -2,9 +2,41 @@
 
 #ifdef PLATFORM_WINDOWS
 
-socket_t createSocket(int domain, int type, int protocol)
+struct sockaddr_in createSockaddrIn(int port, const char *ipAddress)
 {
-  socket_t sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  struct sockaddr_in addr;
+  memset(&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+
+  if (inet_pton(AF_INET, ipAddress, &addr.sin_addr) <= 0)
+  {
+    printf("Invalid IP address: %s\n", ipAddress);
+    addr.sin_addr.s_addr = INADDR_ANY;
+  }
+
+  addr.sin_port = htons(port);
+  return addr;
+}
+
+int platformInit()
+{
+  WSADATA wsaData;
+  if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+  {
+    printf("WSAStartup failed with error: %d\n", WSAGetLastError());
+    return PLATFORM_FAILURE;
+  }
+  return PLATFORM_SUCCESS;
+}
+
+void platformCleanup()
+{
+  WSACleanup();
+}
+
+socket_t createSocket(int type, int protocol)
+{
+  socket_t sock_fd = socket(AF_INET, type, protocol);
   if (sock_fd == INVALID_SOCKET)
   {
     printf("Socket creation failed\n");
@@ -26,24 +58,75 @@ int bindSocket(socket_t sockfd, const struct sockaddr *addr, socklen_t addrlen)
   return PLATFORM_SUCCESS;
 }
 
-int listenSocket(socket_t sockfd, int backlog)
+int listenSocket(socket_t sockfd, int maxClients)
 {
+  if (listen(sockfd, maxClients) == SOCKET_ERROR)
+  {
+    printf("Listen failed\n");
+    closesocket(sockfd);
+    WSACleanup();
+    return PLATFORM_FAILURE;
+  }
+  return PLATFORM_SUCCESS;
 }
 
 socket_t acceptSocket(socket_t sockfd, struct sockaddr *addr, socklen_t *addrlen)
 {
+  socket_t clientSocket = accept(sockfd, addr, addrlen);
+  if (clientSocket == INVALID_SOCKET)
+  {
+    printf("Accept failed\n");
+    return PLATFORM_FAILURE;
+  }
+  return clientSocket;
 }
 
 int connectSocket(socket_t sockfd, const struct sockaddr *addr, socklen_t addrlen)
 {
+  if (connect(sockfd, addr, addrlen) == SOCKET_ERROR)
+  {
+    printf("Connect failed. Error code: %d\n", WSAGetLastError());
+    return PLATFORM_FAILURE;
+  }
+  return PLATFORM_SUCCESS;
 }
 
 int sendData(socket_t sockfd, const void *buf, size_t len, int flags)
 {
+  if (sockfd == INVALID_SOCKET)
+  {
+    return PLATFORM_FAILURE;
+  }
+
+  if (send(sockfd, buf, len, flags) == SOCKET_ERROR)
+  {
+    printf("Send failed to client socket %d. Error: %d\n", sockfd, WSAGetLastError());
+    return PLATFORM_FAILURE;
+  }
+  return PLATFORM_SUCCESS;
 }
 
 int receiveData(socket_t sockfd, void *buf, size_t len, int flags)
 {
+  if (sockfd == INVALID_SOCKET)
+  {
+    return PLATFORM_FAILURE;
+  }
+
+  int bytesReceived = recv(sockfd, buf, len, flags);
+  if (bytesReceived == SOCKET_ERROR)
+  {
+    printf("Receive failed from socket %d. Error: %d\n", sockfd, WSAGetLastError());
+    return PLATFORM_FAILURE;
+  }
+
+  if (bytesReceived == 0)
+  {
+    printf("Connection closed by client on socket %d\n", sockfd);
+    return PLATFORM_CONNECTION_CLOSED;
+  }
+
+  return bytesReceived;
 }
 
 int platformGetLastError()
@@ -64,6 +147,16 @@ void shutdownWrite(socket_t sockfd)
 void shutdownBoth(socket_t sockfd)
 {
   shutdown(sockfd, SD_BOTH);
+}
+
+int closeSocket(socket_t sockfd)
+{
+  if (closesocket(sockfd) == SOCKET_ERROR)
+  {
+    printf("Socket closure failed. Error: %d\n", WSAGetLastError());
+    return PLATFORM_FAILURE;
+  }
+  return PLATFORM_SUCCESS;
 }
 
 #endif
--- main.c
+++ main.c
@@ -1,9 +1,56 @@
 #include <stdio.h>
+#include "platform.h"
 
 int main()
 {
-  char msg[] = "Hello World!";
-  printf("%s\n", msg);
-  getchar();
+  if (platformInit() == PLATFORM_FAILURE)
+  {
+    printf("Failed to init. Exiting...");
+    return 1;
+  }
+
+  socket_t socket = createSocket(SOCK_STREAM, IPPROTO_TCP);
+
+  if (socket == PLATFORM_FAILURE)
+  {
+    printf("Failed to create socket. Exiting...");
+    return 1;
+  }
+
+  struct sockaddr_in addr = createSockaddrIn(8080, "0.0.0.0");
+  if (bindSocket(socket, (struct sockaddr *)&addr, (socklen_t)sizeof(addr)) == PLATFORM_FAILURE)
+  {
+    printf("Failed to bind socket. Exiting...");
+    return 1;
+  }
+
+  if (listenSocket(socket, 10) == PLATFORM_FAILURE)
+  {
+    printf("Failed to listen. Exiting...");
+    return 1;
+  }
+
+  printf("Listening on port 8080...");
+
+  struct sockaddr_in clientAddr;
+  int clientLen = sizeof(clientAddr);
+
+  while (1)
+  {
+    socket_t clientSocket = accept(socket, (struct sockaddr *)&clientAddr, &clientLen);
+    if (clientSocket == PLATFORM_FAILURE)
+    {
+      printf("Accept failed\n");
+      continue;
+    }
+
+    char msg[] = "Hello World!";
+
+    sendData(clientSocket, msg, sizeof(msg) - 1, 0);
+
+    closeSocket(clientSocket);
+  }
+
+  platformCleanup();
   return 0;
 }
